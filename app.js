ac687feb2a028b1ff01860b84ef4cfe32f7f6f82
Sharvez Geter <sharvezgeter@gmail.com>
2023-01-31 15:12:47 -0500

    test

---
+++ app.js
@@ -0,0 +1,108 @@
+
+const sampleBlogs = [
+  {
+		title: "dicta",
+    text: "Iusto et in et. Nulla accusantium fugit. Et qui dolorem inventore soluta et veritatis. Aut ut aut non laudantium eveniet suscipit odit. Sapiente sint nihil nihil sit et molestias. In nisi omnis quas et sed aut minus aperiam ea.\n \rLaudantium quo quisquam quae. Et et quas officia perspiciatis iusto sunt sunt eaque. Quidem sit voluptas deserunt sequi magni.\n \rEst est facere cumque ipsam omnis animi. Voluptatem magnam officiis architecto possimus. Quia similique aut eos qui. Quasi quae sed aliquam.",
+    author: "Darren Abbott",
+    category: ["Lorem", "sit", "amet"],
+    createdAt: "2022-03-22T10:36:37.176Z",
+    lastModified: "2022-03-22T10:36:37.176Z",
+  },
+  {
+		title: "ducimus",
+    text: "Placeat ea et fuga. Qui itaque quibusdam nam. Maxime nobis quam. Et laudantium sunt incidunt reiciendis.\n \rEarum aut sed omnis autem aliquam architecto corporis sint. Nostrum cumque voluptatem aperiam alias similique. Tenetur et esse omnis praesentium ipsum alias. Impedit rerum qui quia quaerat architecto mollitia est autem. Qui blanditiis earum et qui dolorum reprehenderit. Debitis est temporibus.\n \rEt nam sed. Corporis ut rerum. Ut qui dolore est dolorem ex.",
+    author: "Luke Rogahn PhD",
+    category: ["Lorem", "ipsum"],
+    createdAt: "2022-03-22T15:16:56.285Z",
+    lastModified: "2022-03-22T15:16:56.285Z",
+  },
+  {
+		title: "quod",
+    text: "Accusamus nisi eos. Tenetur earum tenetur nemo. Qui voluptas temporibus repellendus maxime. Ipsum optio voluptate enim nihil. Ea et dolorem. Omnis unde perspiciatis.\n \rUt odio eaque. Harum non placeat. Eveniet molestiae in cupiditate dolor doloremque rerum eligendi aut ab.\n \rMolestias eligendi et. Nemo velit natus autem numquam atque provident et nulla. In et dolores ad nihil. Delectus quis doloremque asperiores similique. Asperiores id nam vitae nobis labore autem. Dolor aperiam provident quia consectetur aut ut.",
+    author: "Maryann Schneider",
+    category: ["Lorem", "ipsum", "dolor", "sit", "amet"],
+    createdAt: "2022-03-21T20:09:32.298Z",
+    lastModified: "2022-03-21T20:09:32.298Z",
+  },
+  {
+		title: "ut",
+    text: "Itaque necessitatibus repudiandae. Porro suscipit exercitationem qui atque. Perferendis suscipit debitis sint aut dignissimos nobis ut. Modi ea nihil est vel consequuntur voluptatem. In magnam delectus in eos reiciendis sit est enim eligendi. Sint dicta at.\n \rConsectetur aspernatur alias sed non explicabo blanditiis laborum fugit voluptate. Reiciendis iste aut sit natus qui et in ratione. Placeat qui in voluptatum autem nulla ratione. Commodi sit alias sint sapiente rem. Quia sapiente minus deleniti vitae.\n \rExercitationem numquam omnis maxime dolorum sed deserunt suscipit laudantium. Ad et autem voluptatem esse laudantium et. Id fuga accusamus est sapiente dicta.",
+    author: "Dr. Lorenzo Anderson",
+    category: ["ipsum", "dolor", "sit", "amet"],
+    createdAt: "2022-03-21T23:07:53.447Z",
+    lastModified: "2022-03-21T23:07:53.447Z",
+  },
+  {
+		title: "id",
+    text: "Porro officia aliquid fugiat sed reprehenderit illo amet doloribus sed. Molestiae vero et. Quae voluptates dolores. Voluptatem facere fuga. Veniam perferendis illo ut sunt earum deleniti.\n \rIusto neque dolorem esse error. Saepe et quia ut corrupti. Autem repellendus similique dolorem sunt in ipsa perferendis. Et excepturi ut voluptatem deserunt accusantium dolores aperiam cum ut.\n \rDoloremque expedita sit et voluptatem unde libero. Numquam beatae sed repellat iusto doloribus fugit tenetur. Possimus et ut adipisci harum voluptatem provident consequatur. Corporis quo aut vel itaque blanditiis illum.",
+    author: "Bobbie Dach",
+    category: ["amet"],
+    createdAt: "2022-03-22T15:14:39.819Z",
+    lastModified: "2022-03-22T15:14:39.819Z",
+  },
+];
+
+
+
+
+
+
+
+
+//Instatiate standard libraries
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+//Setup router for routes
+//importing from routes/
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const blogsRouter = require('./routes/blogs');
+
+//instantiate the actual express app
+const port = 5001;
+const app = express();
+
+// view engine setup
+//sets environment variables (things we can access across the application)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//associating the libraries with the app
+// adding middleware
+//(adding libraries that we can use throughout our application)
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//for hosting static files: css, html, images etc.
+app.use(express.static(path.join(__dirname, 'public')));
+
+//we bind the routers to routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/blogs', blogsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(port, ()=>{
+  console.log(`Example app listening on port ${port}`)
+})
+module.exports = app;
